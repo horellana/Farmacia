36855f49a68765efd0e057985c09c553f36909e7
Hector Orellana <pulga.p.29@gmail.com>
2016-12-04 18:48:01 -0300

    boton iniciar sesion

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'farmacia#index'
 
+  resources :user
   scope :devise do
-    devise_for :users
+    devise_for :user
   end
 end
