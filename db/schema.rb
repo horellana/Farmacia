33792659015e3d573405fd72f9aa3aa84925e00d
Hector Orellana <pulga.p.29@gmail.com>
2016-12-05 13:03:02 -0300

    mas trabajo en venta de productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204184015) do
+ActiveRecord::Schema.define(version: 20161205150329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,8 +80,16 @@ ActiveRecord::Schema.define(version: 20161204184015) do
     t.string   "isp"
     t.integer  "category_id"
     t.integer  "discount"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.integer  "quantity"
+    t.integer  "item_id"
+    t.string   "item_type"
+    t.integer  "price_cents",             default: 0,     null: false
+    t.string   "price_currency",          default: "USD", null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.string   "name"
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
     t.index ["dose_id"], name: "index_products_on_dose_id", using: :btree
     t.index ["medicinal_ingredient_id"], name: "index_products_on_medicinal_ingredient_id", using: :btree
@@ -113,6 +121,11 @@ ActiveRecord::Schema.define(version: 20161204184015) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.index ["checkout_id"], name: "index_quotations_on_checkout_id", using: :btree
+  end
+
+  create_table "shopping_carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "transactions", force: :cascade do |t|
