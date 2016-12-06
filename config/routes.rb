952ddaccda7d0e82de9599da04ce70a121edec2b
Nicolas Toro <nicorasengan@gmail.com>
2016-12-06 18:54:53 +0000

    autocompletar PA en crear producto

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   # get 'search#products', to: :search_product, controller: 'search', action: :products, as: 'search_products'
 
   resources :products do
-    get :autocomplete_product_name, :on => :collection
+    get :autocomplete_product_name, on: :collection
   end
+  
+  resources :medicinal_ingredient do
+    get :autocomplete_medicinal_ingredient_name, on: :collection
+  end
+  
   resources :transactions
   resources :products
 
