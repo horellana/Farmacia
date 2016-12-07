700f1356e4daf5191ecd4ef2e5db94fb25174897
Francisco_Europeo <francisco.iri_95@hotmail.es>
2016-12-07 17:15:59 +0000

    Creando cliente / vista basica

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,32 @@
+class ClientsController < ApplicationController
+  autocomplete :client, :name
+  before_action :authenticate_user!
+
+  def new
+    @client = Client.new
+  end
+
+  def index
+    @client = Client.all
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+
+  def create
+        @client = Client.new(name: params[:client][:name],
+                        lastname: params[:client][:lastname],
+                        rut: params[:client][:rut],
+                        email: params[:client][:email],
+                        address: params[:client][:address],
+                        )
+        if @client.save
+              redirect_to @client
+        else
+            
+        end
+  end
+
+end
