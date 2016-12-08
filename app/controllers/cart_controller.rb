84244c52556576eb0a918abf9359a24f98661a64
Hector Orellana <pulga.p.29@gmail.com>
2016-12-08 18:25:39 -0300

    nuevos modelos para el carrito de compras

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,30 @@
+class CartController < ApplicationController
+  before_action :authenticate_user!
+  before_action :setup_cart
+
+  def new
+    redirect_to @cart
+  end
+
+  def show
+  end
+
+  def update
+    query = params[:search_product_field]
+    product = Product.find_by name: query
+    @cart.add(product)
+
+    redirect_to @cart
+  end
+
+  private
+
+  def setup_cart
+    begin
+      @cart = Cart.find session[:cart_id]
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create!
+      session[:cart_id] = @cart.id
+    end
+  end
+end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  def increase_quantity
+    self.quantity = self.quantity + 1
+    self.save!
+  end
 end
