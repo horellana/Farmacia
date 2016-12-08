bdbe21de9c63a7717dc7d3a2d3f644531356f02a
Hector Orellana <pulga.p.29@gmail.com>
2016-12-08 14:31:24 -0300

    vista buscar aun no funciona

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
-  acts_as_shopping_cart_item_for :shopping_cart
-
   belongs_to :provider
   belongs_to :medicinal_ingredient
   belongs_to :dose
--- app/models/shopping_cart.rb
+++
@@ -1,7 +0,0 @@
-class ShoppingCart < ApplicationRecord
-  acts_as_shopping_cart_using :product
-
-  def tax_pct
-    0
-  end
-end
