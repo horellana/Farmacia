786f5cdc9c7d4b30dab49f8726c83f6e3e4eba5c
Hector Orellana <pulga.p.29@gmail.com>
2016-12-08 18:26:51 -0300

    metodo para mostrar el precio real de un producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@ class Product < ApplicationRecord
   belongs_to :dose
   belongs_to :category
 
+  def price
+    sale_price - discount
+  end
+
   def profit
     sale_price - purchase_price - discount
   end
