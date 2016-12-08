cc55f04ca13a3634a21869fb6230608ca231dec5
Hector Orellana <pulga.p.29@gmail.com>
2016-12-08 01:31:51 -0300

    metodo profit

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ApplicationRecord
   belongs_to :medicinal_ingredient
   belongs_to :dose
   belongs_to :category
+
+  def profit
+    sale_price - purchase_price - discount
+  end
 end
