7b89ee3b4831f80ed0b2a283a901f73c76ff8238
Hector Orellana <pulga.p.29@gmail.com>
2016-12-09 02:21:59 -0300

    ordenar productos segun ganancia

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,11 @@ class Product < ApplicationRecord
   belongs_to :dose
   belongs_to :category
 
+  def self.order_by_profit
+    select('*, sale_price - purchase_price as price')
+      .order('price desc')
+  end
+
   def price
     sale_price - discount
   end
