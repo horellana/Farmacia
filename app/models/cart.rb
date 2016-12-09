33a12e47f2f573a5bbfa9fbd0df1864683630a55
hector orellana <pulga.p.29@gmail.com>
2016-12-09 14:21:41 -0500

    no se pueden vender carritos vacios

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,10 @@
 class Cart < ApplicationRecord
   has_many :cart_items
 
+  def empty?
+    items.length == 0
+  end
+
   def add(product)
     item = CartItem.find_by cart: self, product: product
 
