9e1f9036bd9399f149534c5704ab2f80bb5d9c5c
Francisco_Europeo <francisco.iri_95@hotmail.es>
2016-12-09 18:47:25 +0000

    principio activo controlador y vista sin provar / dropdown con problemas

--- app/controllers/medicinal_ingredient_controller.rb
+++ app/controllers/medicinal_ingredient_controller.rb
@@ -1,3 +1,23 @@
 class MedicinalIngredientController < ApplicationController
      autocomplete :medicinal_ingredient, :name
+     before_action :authenticate_user!
+
+  def new
+    @medicinal_ingredient = Medicinal_ingredient.new
+  end
+
+  def index
+    @medicinal_ingredient = Medicinal_ingredient.all
+  end
+
+  def show
+    @medicinal_ingredient = Medicinal_ingredient.find(params[:id])
+  end
+
+
+  def create
+        @medicinal_ingredient = Medicinal_ingredient.new(name: params[:medicinal_ingredient][:name],
+                        )
+        @medicinal_ingredient.save!
+  end
 end
