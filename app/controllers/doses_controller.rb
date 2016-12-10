966d61c4e3427e28108a793300d0b64ae9d95dcc
Nicolas Toro <nicorasengan@gmail.com>
2016-12-10 20:19:35 +0000

    crear dosis funcionando

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+     autocomplete :Category, :name
+     before_action :authenticate_user!
+
+  def new
+    @dose = Dose.new
+  end
+
+  def index
+    @dose = Dose.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+
+  def create
+        @dose = Dose.new(kind: params[:dose][:kind],
+                         quantity: params[:dose][:quantity],)
+           if @dose.save
+            flash[:notice] = "Dosis Creada."
+            redirect_to @dose
+          else
+            render :action => 'new'
+          end
+   end
+ 
+end
