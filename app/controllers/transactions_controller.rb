851c69d40be87ffd3d2ce1ac689946dee13e9b4c
Hector Orellana <pulga.p.29@gmail.com>
2016-12-10 22:05:27 -0300

    transaction#create

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,35 +1,51 @@
 class TransactionsController < ApplicationController
   before_action :setup_cart
+  before_action :avoid_empty_cart, only: :create
 
   def show
     @transaction = Transaction.find params[:id]
   end
 
   def create
-    avoid_empty_cart
-
     now = Time.now
     @transaction = Transaction.new
+
+    @transaction.date = now
+    @transaction.iva = 0.19
+
+    if params[:client_rut]
+      puts "CLIENTE #{params[:client_rut]}"
+      begin
+        puts "SETEANDO CLIENTE"
+        client = Client.find_by! rut: params[:client_rut]
+        client.transactions << @transaction
+      rescue ActiveRecord::RecordNotFound
+        puts "CLIENTE NO ENCONTRADO"
+        client = Client.new
+        client.rut = params[:client_rut]
+        puts "GRABANDO NUEVO CLIENTE"
+        client.save!
+        @transaction.client = client
+      end
+
+      puts "EL CLIENTE ES: #{@transaction.client}"
+    end
+
+    @transaction.save!
 
     @cart.items.each do |item|
       td = TransactionDetail.new
       td.product = item.product
-      td.transactionn = @transaction
       td.quantity = item.quantity
-      td.unit_price = item.product.sale_price
-      td.net_price = item.product.price
+      td.unit_price = item.product.price
+      td.net_price = item.product.price - (item.product.price * 0.19)
       td.discount = item.product.discount
       td.devolution = 'no'
       td.update_time = now
+      td.user = current_user
+      td.transactionn = @transaction
+      td.save!
     end
-
-    # @transaction.kind = 'nose'
-    @transaction.iva = 0.19
-    # @transaction.net_amount = @transaction.calc_net_amount
-    # @transaction.discount = @transaction.calc_discount
-    # @transaction.total_amount = @transaction.calc_total_amount
-
-    @transaction.save!
 
     # el carrito de compras ya no es necesario
     session[:cart_id] = nil
@@ -42,7 +58,7 @@ class TransactionsController < ApplicationController
   def avoid_empty_cart
     if @cart.empty?
       flash[:alert] = 'El carrito esta vacio!'
-      redirect_to :back
+      return redirect_to :back
     end
   end
 end
