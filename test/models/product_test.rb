0c307d52964af9bcce532aab8688b20ef78e3cca
Hector Orellana <pulga.p.29@gmail.com>
2016-12-11 18:53:11 -0300

    2 test para el modelo del producto

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Los productos calculan su precio final' do
+    product = Product.find_by! name: 'Paracetamol'
+    expected = product.sale_price - product.discount
+    assert_equal expected, product.price
+  end
+
+  test 'Los productos calculan su ganancia' do
+    product = Product.find_by! name: 'Paracetamol'
+    expected = product.price - product.purchase_price
+    assert_equal expected, product.profit
+  end
 end
