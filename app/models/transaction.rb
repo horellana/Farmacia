7d3d387cec0d47b38ddecff13da2e53576ccade5
Hector Orellana <pulga.p.29@gmail.com>
2016-12-10 22:05:11 -0300

    relacion cliente transaccion

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+  belongs_to :client
+  has_many :details, class_name: 'TransactionDetail'
+
+  def total
+    details.map { |d| d.net_price * d.quantity }.inject(0, :+)
+  end
 end
