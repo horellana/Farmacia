85b55dab23267fd47549589680ee120eda8d754e
Hector Orellana <pulga.p.29@gmail.com>
2016-12-19 00:52:41 -0300

    mas tests

--- app/controllers/search_product_controller.rb
+++ app/controllers/search_product_controller.rb
@@ -1,4 +1,5 @@
 class SearchProductController < ApplicationController
+  before_action :authenticate_user!
   before_action :setup_cart
 
   def index
--- test/controllers/search_product_controller_test.rb
+++ test/controllers/search_product_controller_test.rb
@@ -1,14 +1,32 @@
 require 'test_helper'
 
 class SearchProductControllerTest < ActionDispatch::IntegrationTest
-  test "deberia mandar al login si falta iniciar sesion" do
-    get search_product_index_url
-    assert_response :redirect
-    assert_redirected_to new_session_path
+  test "no funciona sin iniciar sesion" do
+    get search_product_index_url, xhr: true
+    assert_response :unauthorized
   end
 
-  test "should get index" do
-    get search_product_index_url, params: {search_product_field: 'pa'}, xhr: true
+  test "encuentra al producto correcto" do
+    user = users :one
+
+    sign_in user
+
+    get search_product_index_url,
+        params: {search_product_field: 'pa'},
+        xhr: true
+
     assert_response :success
+
+    assert_match /Paracetamol/, @response.body,
+                 'encuentra productos sin tener el nombre completo'
+
+    get search_product_index_url,
+        params: {search_product_field: 'ibuprofeno'},
+        xhr: true
+
+    assert_match /Ibuprofeno/, @response.body,
+                 'encuenta productos segun nombre'
+
+    sign_out(user)
   end
 end
