5dbcac98d4803ff34bea203fa24faf9d29f648bc
Hector Orellana <pulga.p.29@gmail.com>
2016-12-19 00:52:41 -0300

    mas tests

--- test/controllers/rut_controller_test.rb
+++ test/controllers/rut_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class RutControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "no funciona sin iniciar sesion" do
+    put set_rut_path, params: { data: '11111111-1' }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'devise/test_helpers'
 
 class ActiveSupport::TestCase
+  include Devise::Test::IntegrationHelpers
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
