690107c4cecac2c0637a3799ab71fe607edd1b7b
Hector Orellana <pulga.p.29@gmail.com>
2016-12-30 16:34:21 -0300

    crear producto funcionando

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,14 +15,38 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def create
+    category = Category.find_or_create_by! description: params[:category_description]
+    ingredient = MedicinalIngredient.find_or_create_by! name: params[:medicinal_ingredient_name]
+    provider = Provider.find_or_create_by! name: params[:provider_name]
+    dose = Dose.find_or_create_by! kind: params[:dose_kind]
 
-  def product_param
-    params.require(:product).permit(:name, :description, :sale_price, :purchase_price, :exempt, :commission, :be,
-                                    :isp, :discount, :description, :dose_id, :category_id, :medicinal_ingredient_id, :provider_id )
+    @product = Product.new name: params[:product][:name],
+                           description: params[:product][:description],
+                           sale_price: params[:product][:sale_price],
+                           purchase_price: params[:product][:purchase_price],
+                           exempt: params[:product][:exempt],
+                           commission: params[:product][:commision],
+                           be: params[:product][:be],
+                           isp: params[:product][:isp],
+                           discount: params[:product][:discount]
+
+    @product.category = category
+    @product.medicinal_ingredient = ingredient
+    @product.provider = provider
+    @product.dose = dose
+
+    @product.save!
+
+    redirect_to @product
   end
 
-  def create
-    @product = Product.new(product_param)
-    @product.save!
+  private
+
+  def product_param
+    params
+      .require(:product)
+      .permit(:name, :description, :sale_price, :purchase_price,
+              :exempt, :commission, :be, :isp, :discount, :description)
   end
 end
