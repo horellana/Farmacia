604c41c0d0c6a3ccb0d3ace8067bb1e000d3725d
Bastian <basti.castillodiaz@hotmail.com>
2017-02-21 17:23:58 -0300

    validado direccion y nombre negocio

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,10 @@
 class Provider < ApplicationRecord
 
   validates :name, presence: true
-
+  validates :rut, presence: true
+  validates :business_name, presence: true
+  validates :address, presence: true
+  
   scope :match_name, ->(name) do
     where('name ilike ?', "%#{name}%")
   end
