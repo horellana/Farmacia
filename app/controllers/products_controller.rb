636af82cdc25a18efaf2741d2f5f35d14b5822d9
Francisco_Europeo <francisco.iri_95@hotmail.es>
2017-02-27 01:56:28 +0000

    editar productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -51,9 +51,32 @@ class ProductsController < ApplicationController
     end
   end
 
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to product_path
+  end
+
+
+
   private
 
-  def product_param
+  def product_params
     params
       .require(:product)
       .permit(:name, :description, :sale_price, :purchase_price,
