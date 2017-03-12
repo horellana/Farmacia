5eac7c0229b5ef14b5fcd93fe8fdbabc168ef5f7
juiko <juiko@debian>
2017-03-12 13:49:49 -0700

    Arreglada busqueda de productos

--- app/controllers/search_product_controller.rb
+++ app/controllers/search_product_controller.rb
@@ -3,7 +3,7 @@ class SearchProductController < ApplicationController
   before_action :setup_cart
 
   def index
-    query = params[:search_product_field]
+    query = params[:product_name]
     @products = Product.match_name(query)
     respond_to :js
   end
