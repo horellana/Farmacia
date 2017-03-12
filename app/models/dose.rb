2423754f03bd32f987df33bf3b5dc8dc9cd056c2
Bastian <basti.castillodiaz@hotmail.com>
2017-03-12 19:03:41 -0300

    gema rutchileno instalada

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,7 @@
 class Dose < ApplicationRecord
 
-  validates :kind, presence: true
-  validates :quantity, presence: true
+  validates :kind, presence: {case_sensitive: false ,message: "no puede estar vacio"}
+  validates :quantity, presence: {case_sensitive: false ,message: "no puede estar vacio"}
 
   scope :match_kind, ->(kind) do
     where('kind ilike ?', "%#{kind}%")
