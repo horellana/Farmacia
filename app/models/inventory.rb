a1b63ca22fe2d7be819e6f83a04412c08b830321
juiko <juiko@debian>
2017-03-12 17:28:45 -0700

    mensaje al eliminar un cliente

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,11 +1,3 @@
-class StockValidator < ActiveModel::Validator
-  def validate(record)
-    if record.stock and record.stock < 0
-      record.errors[:stock] << 'El stock no puede ser menor a 0!'
-    end
-  end
-end
-
 class Inventory < ApplicationRecord
   belongs_to :office
   belongs_to :product
