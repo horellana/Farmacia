9258419bfbd089b1b07d93e38c32f0ae8b19f58c
juiko <juiko@debian>
2017-03-12 17:01:17 -0700

    validacion para el nombre

---
+++ app/validators/name_validator.rb
@@ -0,0 +1,15 @@
+class NameValidator < ActiveModel::Validator
+  def validate(record)
+    if solo_numeros(record.name)
+      record.errors[:nombre] << 'no puede tener solo numeros'
+
+    elsif record.lastname && solo_numeros(record.lastname)
+      record.errors[:nombre] << 'no puede tener solo numeros'
+    end
+  end
+
+  private
+  def solo_numeros(str)
+    str.chars.all? { |c| [0, 1, 2, 3, 4, 5, 6, 7 ,8 ,9].map(&:to_s).include?(c) }
+  end
+end
