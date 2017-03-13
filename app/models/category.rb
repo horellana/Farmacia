799a8dad5d0eb0436dde24df0c80c7e6998427c3
Bastian <basti.castillodiaz@hotmail.com>
2017-03-12 21:51:14 -0300

    validados dosis, categoria y ingrediente medicinal

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :products, inverse_of: :category
 
+validates :description, presence: {case_sensitive: false ,message: "no puede estar vacio"}
 
 
   scope :match_description, ->(description) do
