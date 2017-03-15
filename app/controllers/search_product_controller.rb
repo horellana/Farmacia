dca8d2d7bcab21f41de9463662fa679539842406
Hector Orellana <pulga.p.29@gmail.com>
2017-03-15 15:38:16 -0700

    mostrar bioequivalentes en la busqueda de producto

--- app/controllers/search_product_controller.rb
+++ app/controllers/search_product_controller.rb
@@ -3,18 +3,27 @@ class SearchProductController < ApplicationController
   before_action :setup_cart
 
   def index
-    query = params[:product_medicinal_ingredient]
+    query = params[:product_name]
 
-    puts "query = #{query}"
-
-    mi = MedicinalIngredient.find_by name: query
-    puts "medicinal_ingredient found = #{mi}"
-
-
-    @products = Product.where medicinal_ingredient: mi
-
-    puts "@products = #{@products}"
+    @product = Product.find_by(name: query)
+    @equivalents = Product
+                   .where(medicinal_ingredient: @product.medicinal_ingredient)
+                   .where.not(id: @product.id)
 
     respond_to :js
+
+    # query = params[:product_medicinal_ingredient]
+
+    # puts "query = #{query}"
+
+    # mi = MedicinalIngredient.find_by name: query
+    # puts "medicinal_ingredient found = #{mi}"
+
+
+    # @products = Product.where medicinal_ingredient: mi
+
+    # puts "@products = #{@products}"
+
+    # respond_to :js
   end
 end
