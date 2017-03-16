77d9b050813f0ee97662a7766983be1530e01541
Hector Orellana <pulga.p.29@gmail.com>
2017-03-16 14:16:09 -0700

    relacion producto, principio y tabla pivote

--- app/models/principle.rb
+++ app/models/principle.rb
@@ -1,6 +1,7 @@
 class Principle < ApplicationRecord
+  has_many :products, through: :principle_details
 
-validates :name, presence: {case_sensitive: false ,message: "no puede estar vacio"}
+  validates :name, presence: {case_sensitive: false ,message: "no puede estar vacio"}
 
   scope :match_name, ->(name) do
     where('name ilike ?', "%#{name}%")
