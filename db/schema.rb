c031f0db53c4c2df13d0fde5989b83856bf867bc
Hector Orellana <pulga.p.29@gmail.com>
2017-03-15 18:46:13 -0700

    Agregado el codigo de los productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315002112) do
+ActiveRecord::Schema.define(version: 20170316000900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20170315002112) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "name"
+    t.string   "code"
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
     t.index ["dose_id"], name: "index_products_on_dose_id", using: :btree
     t.index ["medicinal_ingredient_id"], name: "index_products_on_medicinal_ingredient_id", using: :btree
