087cbcf5141394ddef0c4db4206d4d44b25a42d9
Hector Orellana <pulga.p.29@gmail.com>
2017-03-16 08:25:44 -0700

    Documentos, borrar carrito (Incompleto)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
-  has_many :cart_items
+  has_many :cart_items, dependent: :delete_all
   belongs_to :transactionn,
              required: false,
              class_name: 'Transaction',
