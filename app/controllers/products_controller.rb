9993188cf5de828d4fc341c382535d7a6e3f1345
Hector Orellana <pulga.p.29@gmail.com>
2017-03-16 10:06:22 -0700

    ???

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,35 @@ class ProductsController < ApplicationController
   end
 
   def create
+<<<<<<< HEAD
     @product = Product.new
     @product = create_from_form(@product, params)
+=======
+    category = Category.find_by description: params[:category_descripcion]
+    ingredient = MedicinalIngredient.find_by name: params[:product][:medicinal_ingredient_id]
+    provider = Provider.find_by name: params[:product][:provider_id]
+    presentation = presentation.find_by kind: params[:product][:presentation_id]
+
+    inventory = Inventory.new stock: params[:stock],
+                              minimum_stock: params[:minimum_stock],
+                              office: current_office
+
+    @product = Product.new name: params[:product][:name],
+                           description: params[:product][:description],
+                           sale_price: params[:product][:sale_price],
+                           purchase_price: params[:product][:purchase_price],
+                           be: params[:product_be],
+                           isp: params[:product_isp],
+                           discount: params[:product][:discount].to_i
+
+    @product.isp = false if not @product.isp
+
+    @product.category = category
+    @product.medicinal_ingredient = ingredient
+    @product.provider = provider
+    @product.presentation = presentation
+    @product.inventory = inventory
+>>>>>>> b6b59ecb1a7a4f6fe9636cc0b9a3439f3bcdbbf4
 
     if @product.save
       redirect_to @product
--- test/models/dose_test.rb
+++ test/models/dose_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DoseTest < ActiveSupport::TestCase
+class presentationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
