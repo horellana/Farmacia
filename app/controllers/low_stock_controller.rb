038bfda8340c5122ee8a45bd4a8690e28ab2ee8a
Hector Orellana <pulga.p.29@gmail.com>
2017-03-17 12:21:39 -0700

    no mostrar el notice siempre

--- app/controllers/low_stock_controller.rb
+++ app/controllers/low_stock_controller.rb
@@ -2,7 +2,7 @@ class LowStockController < ApplicationController
   def index
     @products = Product.low_stock
     if @products.length <= 0
-      flash[:notice] = "Todos los productos estan sobre el stock minimo"
+      flash.now[:notice] = "Todos los productos estan sobre el stock minimo"
     end
-    end
+  end
 end
