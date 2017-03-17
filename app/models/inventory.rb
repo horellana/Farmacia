c31a4ccfe8a5155a022ef2d49ea6d19642374add
Bastian <basti.castillodiaz@hotmail.com>
2017-03-17 17:14:42 -0300

    inventario visible

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -19,10 +19,6 @@ class Inventory < ApplicationRecord
     select('product_id').where('stock <= minimum_stock')
   end
 
-  def total
-    stock * product.sale_price
-  end
-
   def increase_stock(n=1)
     self.stock += n
     save
