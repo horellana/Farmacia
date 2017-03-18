c0db3a57a85d219b1cf78f611582e0e30e5a58cf
Hector Orellana <pulga.p.29@gmail.com>
2017-03-18 12:31:52 -0700

    archivo `app/validators/email_validator.rb`

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,11 +1,3 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    record.errors.add attribute, (options[:message] || "is not an email") unless
-      value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  end
-end
-
-
 class Provider < ApplicationRecord
   has_many :products, inverse_of: :product
 
