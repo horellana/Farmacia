88fd251edaf9ab8a9a8723f58160f41ee5566a29
Bastian <basti.castillodiaz@hotmail.com>
2017-03-22 14:22:17 -0300

    historico precios

--- test/controllers/price_history_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PriceHistoryControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get price_history_index_url
-    assert_response :success
-  end
-
-end
