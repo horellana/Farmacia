5c2326c4954eeb6d11875596d40d46acf72b255e
Bastian <basti.castillodiaz@hotmail.com>
2017-03-23 15:13:04 -0300

    botones transformar a pdf

--- app/controllers/low_stock_controller.rb
+++ app/controllers/low_stock_controller.rb
@@ -7,5 +7,11 @@ class LowStockController < ApplicationController
     if @products.length <= 0
       flash.now[:notice] = "Todos los productos estan sobre el stock minimo"
     end
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "Bajo stock"   # Excluding ".pdf" extension.
+      end
+    end
   end
 end
