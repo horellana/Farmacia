5a19c70d000373ef57b3ce2be3e65eb00e5e938f
Hector Orellana <pulga.p.29@gmail.com>
2017-03-23 11:23:04 -0700

    buscar productos revivido

--- app/controllers/search_products_controller.rb
+++ app/controllers/search_products_controller.rb
@@ -1,16 +1,9 @@
-class SearchProductController < ApplicationController
+class SearchProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :setup_cart
 
   def index
     query = params[:product_name]
     @product, @equivalents = SearchProductService.new(query).call
-
-    puts "@product = #{@product}"
-    puts "@equivalents = #{@equivalents}"
-
-    puts "@product.stock"
-
-    respond_to :js
   end
 end
