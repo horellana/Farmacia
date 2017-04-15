e65a69e837bbce440a5611f8015bec68e7627b88
Hector Orellana <pulga.p.29@gmail.com>
2017-04-14 19:15:45 -0700

    test para editar algunos objetos

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
@@ -13,5 +14,17 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     assert_equal original_category_count + 1, Category.count
     assert_equal 'foo', Category.last.description
+  end
+
+  test "Se puede editar una categoria" do
+    sign_in(users(:one))
+
+    category = Category.first
+    original_description = category.description
+
+    put category_path(category), params: { category: { description: original_description + 'foo' }}
+
+    assert_response :redirect
+    assert_not_equal original_description, Category.first.description
   end
 end
--- test/controllers/principles_controller_test.rb
+++ test/controllers/principles_controller_test.rb
@@ -14,4 +14,16 @@ class PrinciplesControllerTest < ActionDispatch::IntegrationTest
     assert_equal original_principle_count + 1, Principle.count
     assert_equal 'foo', Principle.last.name
   end
+
+  test "Se puede editar un principio activo" do
+    sign_in(users(:one))
+
+    principle = Principle.first
+    original_name = principle.name
+
+    put principle_path(principle), params: { principle: { name: original_name + 'foo' }}
+
+    assert_response :redirect
+    assert_not_equal original_name, Principle.first.name
+  end
 end
